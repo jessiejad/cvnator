79a0e789deef57407f4cac4c01030f38b069fbd0
Jessie Jadas <jessiejadas@gmail.com>
2016-07-01 23:25:39 +0200

    User CV : delete and redirection + Fav button

--- web/app/controlers/userResumeCtrl.js
+++ web/app/controlers/userResumeCtrl.js
@@ -1,15 +1,20 @@
 'use strict';
 
 angular.module('myResumeApp')
-    .controller('userResumeCtrl', function($scope, $http,$mdToast,$stateParams){
+    .controller('userResumeCtrl', function($scope, $http,$mdToast,$stateParams, $mdDialog, $state){
 
+        var resume;
         // ---- Call API to get users resume
         $http.get('http://localhost:3000/myResume').then(function(result){
             console.log(result);
-            if(result == null) {
+            if(result.data == null) {
                 // User n'a pas encore de resume
+                $state.go("accueilCv")
             }
-            $scope.resume = result.data;
+            else {
+                resume = result.data;
+            }
+            $scope.resume = resume;
             $scope.template = result.data.defaultTemplate || 'default';
 
             console.log($scope.resume, $scope.template);
@@ -17,6 +22,33 @@ angular.module('myResumeApp')
             console.log(reason);
             $mdToast.simpleToast("Erreur lors de la récupération du cv")
         });
+
+        var deleteResume = function(id) {
+            // ---- Call API to delete a resume
+            $http.delete('http://localhost:3000/resumes/' + id).then(function(result){
+                console.log(result);
+            }, function(reason){
+                console.log(reason);
+                $mdToast.simpleToast("Erreur lors de la suppression du cv")
+            });
+        };
+
+        $scope.deleteConfirm = function(ev) {
+            var confirm = $mdDialog.confirm()
+                .title('Souhaitez-vous réellement supprimer votre CV ?')
+                .textContent('La suppression de votre CV entraine la suppression de toutes les données associées')
+                .ariaLabel('Jsp')
+                .targetEvent(ev)
+                .ok('Supprimer')
+                .cancel('Annuler');
+            $mdDialog.show(confirm).then(function() {
+                deleteResume(resume._id);
+                console.log("Deleted resume " + resume._id);
+                $state.go("accueilCv");
+            }, function() {
+                // cancel
+            });
+        };
 
         
 
