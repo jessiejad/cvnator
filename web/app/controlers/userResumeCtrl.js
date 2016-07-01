67b3c928cd72da57d4054fb703c484857bfefdb5
Jessie Jadas <jessiejadas@gmail.com>
2016-07-01 15:39:03 +0200

    userResume

---
+++ web/app/controlers/userResumeCtrl.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myResumeApp')
+    .controller('userResumeCtrl', function($scope, $http,$mdToast,$stateParams){
+
+        // ---- Call API to get users resume
+        $http.get('http://localhost:3000/myResume').then(function(result){
+            console.log(result);
+            if(result == null) {
+                // User n'a pas encore de resume
+            }
+            $scope.resume = result.data;
+        }, function(reason){
+            console.log(reason);
+            $mdToast.simpleToast("Erreur lors de la récupération du cv")
+        });
+
+    });
--- web/app/routing.js
+++ web/app/routing.js
@@ -62,6 +62,12 @@ angular.module('myResumeApp')
                 controller  : 'accueilCvCtrl'
             })
 
+            .state('userResume',{
+                url         : '/userResume',
+                templateUrl : 'app/views/template2.html',
+                controller  : 'userResumeCtrl'
+            })
+
             // Provisoire
             .state('testApi', {
                 url : '/testapi',
