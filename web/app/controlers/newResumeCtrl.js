1b7d91524ba203a61333852effb196b42bb5de93
rubyg <fanbatcho@gmail.com>
2016-07-02 13:29:33 +0200

    form resume with clean add previous button

--- web/app/controlers/newResumeCtrl.js
+++ web/app/controlers/newResumeCtrl.js
@@ -4,60 +4,54 @@
 'use strict';
 
 angular.module('myResumeApp')
-    .controller('newResumeCtrl', function ($scope, $http, $mdToast, $stateParams) {
+    .controller('newResumeCtrl', function ($scope, $http, $mdToast, $stateParams, $mdDialog) {
         var idPerson;
-        var educ;
-        var exp;
-        var comp;
-        var hobb;
-        $scope.max = 5;
+
+
+        $scope.max = 4;
         $scope.selectedIndex = 0;
         $scope.nextTab = function() {
             var index = ($scope.selectedIndex == $scope.max) ? 0 : $scope.selectedIndex + 1;
             $scope.selectedIndex = index;
 
         };
+        $scope.PrevTab = function() {
+            var index = ($scope.selectedIndex == $scope.max) ? 0 : $scope.selectedIndex - 1;
+            $scope.selectedIndex = index;
+
+        };
+
+        $scope.showAlert = function(ev) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('Information')
+                    .textContent('Votre Cv a été créé avec succés')
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('OK')
+                    .targetEvent(ev)
+            );
+        };
         $scope.addPerson = function (person,title) {
             console.log(title);
             var nom=document.getElementById("nom").value;
             person.name+=" "+nom;
             $http.post('http://localhost:3000/person', person).then(function (result) {
-                idPerson = result.data;
                 console.log(result);
+                idPerson = result.data;
             }, function (reason) {
                 console.log(reason);
                 $mdToast.simpleToast("Erreur lors de la création d'une personne")
             })
         };
 
-        $scope.addEduc = function (education) {
-            educ = education;
-            console.log(educ);
-
-        };
-        $scope.addExp = function (experience) {
-
-            exp = experience;
-            console.log(exp);
-
-        };
-        $scope.addHobb = function (hobbie) {
-            hobb = hobbie;
-            console.log(hobb);
-
-        };
-
-        $scope.addComp = function (competence) {
-            competence.person=idPerson;
-            comp = competence;
-            console.log(comp);
-           var resume = {
-                "title": "Création d'un cv via le web",
-                "person": "57757fc2b550095c0910d889"
 
 
-            }
-            $http.post('http://localhost:3000/resumes', competence).then(function (result) {
+        $scope.addResume = function (resume) {
+            resume.person=idPerson;
+            console.log(resume);
+            $http.post('http://localhost:3000/resumes', resume).then(function (result) {
                 console.log(result);
             }, function (reason) {
                 console.log(reason);
