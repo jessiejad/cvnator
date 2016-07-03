9ae018de115bf2d719a858cb0a23eee345061d6c
rubyg <fanbatcho@gmail.com>
2016-07-04 01:01:42 +0200

    Multi form within H et C

--- web/app/controlers/newResumeCtrl.js
+++ web/app/controlers/newResumeCtrl.js
@@ -1,27 +1,135 @@
-/**
- * Created by Jessie Jadas & Quentin Lecornu on 30/06/2016.
- */
 'use strict';
 
 angular.module('myResumeApp')
-    .controller('newResumeCtrl', function ($scope, $http, $mdToast, $stateParams, $mdDialog) {
+    .controller('newResumeCtrl', function ($scope, $http, $mdToast, SecurityContext, $mdDialog, $state) {
         var idPerson;
+        var imgElement;
 
+        $scope.resume = {
+            "experiences": [
+                {
+                    "start": null,
+                    "end": null,
+                    "title": "",
+                    "company": "",
+                    "location": "",
+                    "description": [""]
+                }
+            ],
+            "educations": [
+                {
+                    "start": null,
+                    "end": null,
+                    "title": "",
+                    "school": "",
+                    "location": "",
+                    "description": [
+                        ""
+                    ]
+                }
+            ],
+            "hobbies": [
+                {
+                    "type": "",
+                    "content": [
+                        {
+                            "label": "",
+                            "detail": ""
+                        }
+                    ]
+                }
+            ],
+            "competencies": [
+                {
+                    "type": "",
+                    "content": [
+                        {
+                            "label": "",
+                            "level": ""
+                        }
+                    ]
+                }
+            ]
+        };
+        $scope.resume.experiences.push('');
+        $scope.resume.educations.push('');
+        $scope.resume.competencies.push('');
+        $scope.resume.hobbies.push('');
+        $scope.changeExperience = function (index) {
+            if (index == $scope.resume.experiences.length - 1) {
+                $scope.resume.experiences.push('');
+            }
+        };
+        $scope.changeEducation = function (index) {
+            if (index == $scope.resume.educations.length - 1) {
+                $scope.resume.educations.push('');
+            }
+        };
+        $scope.changeCompetence = function (index) {
+            if (index == $scope.resume.competencies.length - 1) {
+                $scope.resume.competencies.push('');
+            }
+        };
+        $scope.changeHobbie = function (index) {
+            console.log($scope.resume.hobbies[0]);
+            if (index == $scope.resume.hobbies.length - 1) {
+                $scope.resume.hobbies.push('');
+            }
+        };
 
+        $scope.ajouter = function () {
+
+            function createThumbnail(file) {
+                var reader = new FileReader();
+
+                reader.addEventListener('load', function () {
+
+                    imgElement = document.createElement('img');
+                    imgElement.style.maxWidth = '150px';
+                    imgElement.style.maxHeight = '150px';
+                    imgElement.src = this.result;
+                    console.log(imgElement);
+                    prev.appendChild(imgElement);
+
+                });
+
+                reader.readAsDataURL(file);
+            }
+
+            var allowedTypes = ['png', 'jpg', 'jpeg', 'gif'],
+                fileInput = document.querySelector('#file'),
+                prev = document.querySelector('#prev');
+
+            fileInput.addEventListener('change', function () {
+
+                var files = this.files,
+                    filesLen = files.length;
+
+                var imgType = files[0].name.split('.');
+                imgType = imgType[imgType.length - 1];
+
+                if (allowedTypes.indexOf(imgType) != -1) {
+                    createThumbnail(files[0]);
+
+                }
+
+            });
+        };
         $scope.max = 4;
         $scope.selectedIndex = 0;
-        $scope.nextTab = function() {
+        $scope.nextTab = function () {
             var index = ($scope.selectedIndex == $scope.max) ? 0 : $scope.selectedIndex + 1;
             $scope.selectedIndex = index;
 
         };
-        $scope.PrevTab = function() {
-            var index = ($scope.selectedIndex == $scope.max) ? 0 : $scope.selectedIndex - 1;
+        $scope.PrevTab = function () {
+            console.log($scope.selectedIndex);
+            var index = ($scope.selectedIndex == 0) ? 0 : $scope.selectedIndex - 1;
             $scope.selectedIndex = index;
 
         };
 
-        $scope.showAlert = function(ev) {
+        $scope.showAlert = function (ev) {
             $mdDialog.show(
                 $mdDialog.alert()
                     .parent(angular.element(document.querySelector('#popupContainer')))
@@ -33,10 +141,15 @@ angular.module('myResumeApp')
                     .targetEvent(ev)
             );
         };
-        $scope.addPerson = function (person,title) {
+        $scope.addPerson = function (person, title) {
             console.log(title);
-            var nom=document.getElementById("nom").value;
-            person.name+=" "+nom;
+            var nom = document.getElementById("nom").value;
+            person.name += " " + nom;
+            if (imgElement != null) {
+
+                var fichier = imgElement.scr;
+                person.photo = fichier;
+            }
             $http.post('http://localhost:3000/person', person).then(function (result) {
                 console.log(result);
                 idPerson = result.data;
@@ -47,12 +160,15 @@ angular.module('myResumeApp')
         };
 
 
-
         $scope.addResume = function (resume) {
-            resume.person=idPerson;
+            resume.person = idPerson;
             console.log(resume);
+            $scope.resume.experiences.splice($scope.resume.experiences.length - 1, 1);
+            $scope.resume.educations.splice($scope.resume.educations.length - 1, 1);
+
             $http.post('http://localhost:3000/resumes', resume).then(function (result) {
                 console.log(result);
+                $state.transitionTo('monResume');
             }, function (reason) {
                 console.log(reason);
                 $mdToast.simpleToast("Erreur lors de la création du cv")
